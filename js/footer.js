a4675dde8ecd165981af9e77b7a716d8d69c2f00
setareh bagheri <bagh0015@algonquinlive.com>
2023-03-14 01:26:55 -0400

    some changes

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+const createFooter =() => {
+    let footer= document.querySelector('footer');
+    footer.innerHTML= `
+    <div class="container">
+    <div class="left">
+      <p>&copy; 2023 Offrang. All rights reserved.</p>
+    </div>
+    <div class="middle">
+      <a href="#" class="social-link"><img src="png/instagram.png" alt="Instagram icon"></a>
+     <a href="#" class="social-link"><img src="png/twitter.png" alt="Facebook icon"></a>
+     <a href="#" class="social-link"><img src="png/facebook.png" alt="Twitter icon"></a>
+     <a href="#" class="social-link"><img src="png/navigation.png" alt="Youtube icon"></a>
+    </div>
+    <div class="right">
+           <a href="#" class="social-link"><img src="png/chat (1).png" alt="Youtube icon"></a>
+    </div>
+  </div>
+    `;
+}
+    createFooter();
---
+++ js/nav.js
@@ -0,0 +1,33 @@
+const createNav =()=> {
+    let nav = document.querySelector('.navbar');
+    nav.innerHTML = `
+    <div class="pink"></div>
+    <div class="header">
+    <div class="nav-bar">
+    <div class="logo">
+            <img src="Logo.svg" alt="Offrang" width=" 120px">
+    </div>
+
+        <div class="nav">
+            <ul>
+            
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><div class="sign-up-button">
+                <img src="png/signup (1).png" alt="">
+                </div>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+    <div class="search">
+        <input type="text" class="search-box" placeholder=" search">
+    </div>
+        </div>
+
+    `;
+}
+createNav();
